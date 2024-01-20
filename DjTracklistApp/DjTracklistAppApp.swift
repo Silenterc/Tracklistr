0cfda33318624c398884b3938f3bab53fd1f59cf
Lukas Zima <zimaluk1@fit.cvut.cz>
2024-01-20 12:02:54 +0100

    Initial Commit

---
+++ DjTracklistApp/DjTracklistAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  DjTracklistAppApp.swift
+//  DjTracklistApp
+//
+//  Created by Lukas Zima on 20.01.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DjTracklistAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
